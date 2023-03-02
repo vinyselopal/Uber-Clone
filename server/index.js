ebcd25b291e08322159e13a714623d08c27e4ac8
viny <viny0698@gmail.com>
2023-03-02 07:05:31 +0530

    initialize socketio client and server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const { Server } = require('socket.io')
 const app = express()
+const io = new Server()
 
+io.on('connection', (socket) => {
+    // drivers connect
+})
 require('dotenv').config()
-const PORT = process.env.serverPort
+const PORT = process.env.httpPort
+const SPORT = process.env.socketioPort
 
+io.listen(SPORT)
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
