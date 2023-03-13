a13a365dfbd620b16ea361f5b5f47cfc368aecf0
viny <viny0698@gmail.com>
2023-03-13 10:29:31 +0530

    generate drivers data

--- server/generateData.js
+++ server/generateData.js
@@ -1,29 +1,56 @@
 const { faker } = require('@faker-js/faker')
-const { connect, rides } = require('./configDB')
+const { connect, rides, drivers } = require('./configDB')
 
 const generateRidesData = async () => {
     await connect()
     const docs = []
     for (let i = 0; i < 50; i++) {
-        const sourceLatlng = faker.address.nearbyGPSCoordinate([12, 77], 100, true)
-        const destinationLatlng = faker.address.nearbyGPSCoordinate([12, 77], 100, true)
+        const sourceLatlng = faker.address.nearbyGPSCoordinate([12, 77], 100, true).map(a => parseInt(a, 10))
+        const destinationLatlng = faker.address.nearbyGPSCoordinate([12, 77], 100, true).map(a => parseInt(a, 10))
         const sourceAddress = faker.address.streetAddress(true)
         const destinationAddress = faker.address.streetAddress(true)
 
         docs.push({
             user_id: 1,
             source: {
-                latlng: sourceLatlng,
+                latlng: {
+                    type: 'Point',
+                    coordinates: sourceLatlng
+                },
                 address: sourceAddress
             },
             destination: {
-                latlng: destinationLatlng,
+                latlng: {
+                    type: 'Point',
+                    coordinates: destinationLatlng
+                },
                 address: destinationAddress
+            }
+
+        })
+    }
+    await rides.insertMany(docs)
+    // await rides.deleteMany({})
+}
+
+const generateDriversData = async () => {
+    await connect()
+    const docs = []
+    for (let i = 0; i < 50; i++) {
+        const location = faker.address.nearbyGPSCoordinate([12, 77], 100, true).map(a => parseInt(a, 10))
+        const id = i
+
+        docs.push({
+            id,
+            location: {
+                coordinates: location,
+                type: 'Point'
             }
         })
     }
-    console.log(docs)
-    await rides.insertMany(docs)
+    await drivers.insertMany(docs)
+    // await drivers.deleteMany({})
 }
 
 generateRidesData()
+generateDriversData()
